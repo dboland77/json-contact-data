f57f4e306b536497274ffdf0c7d360f6ddb10b88
Dave Boland <dboland77@gmail.com>
2021-06-05 21:08:37 +0100

    Added editable component not yet in ts

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,15 +1,104 @@
-import React from "react";
-import {ContactDetails} from "../interfaces/ContactDetails"
+import React, { useState, useRef,Fragment } from "react";
+import { ContactDetails } from "../interfaces/ContactDetails";
+import Editable from "./Editable";
 
-const ContactCard:React.FC<ContactDetails> = ({name,street,email,phone,age}) => {
+const ContactCard: React.FC<ContactDetails> = ({
+  name,
+  street,
+  email,
+  phone,
+  age,
+}) => {
+  const inputRef = useRef(null);
+  const [newName, setName] = useState("");
+  const [newStreet, setStreet] = useState("");
+  const [newEmail, setEmail] = useState("");
+  const [newPhone, setPhone] = useState("");
+  const [newAge, setAge] = useState("");
+
   return (
-    <div>
-      <h1>{name}</h1>
-      <p>{street}</p>
-      <p>{email}</p>
-      <p>{phone}</p>
-      <p>{age}</p>
-    </div>
+    <Fragment>
+      <Editable
+        text={newName}
+        placeholder={name}
+        childRef={inputRef}
+        type="input"
+      >
+        <input
+          ref={inputRef}
+          type="text"
+          name="Name"
+          placeholder={name}
+          value={newName}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </Editable>
+
+      <Editable
+        text={newStreet}
+        placeholder={street}
+        childRef={inputRef}
+        type="input"
+      >
+        <input
+          ref={inputRef}
+          type="text"
+          name="Street"
+          placeholder={street}
+          value={newStreet}
+          onChange={(e) => setStreet(e.target.value)}
+        />
+      </Editable>
+
+      <Editable
+        text={newEmail}
+        placeholder={email}
+        childRef={inputRef}
+        type="input"
+      >
+        <input
+          ref={inputRef}
+          type="text"
+          name="Email"
+          placeholder={email}
+          value={newEmail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </Editable>
+
+      <Editable
+        text={newPhone}
+        placeholder={phone}
+        childRef={inputRef}
+        type="input"
+      >
+        <input
+          ref={inputRef}
+          type="text"
+          name="Phone"
+          placeholder={phone}
+          value={newPhone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+      </Editable>
+
+      <Editable
+        text={newAge}
+        placeholder={age?.toString()}
+        childRef={inputRef}
+        type="input"
+      >
+        <input
+          ref={inputRef}
+          type="text"
+          name="Age"
+          placeholder={age?.toString()}
+          value={newAge}
+          onChange={(e) => setAge(e.target.value)}
+        />
+      </Editable>
+
+    </Fragment>
   );
 };
 
